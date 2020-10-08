489ac41a7ba79a7db9072ab4b7a23b9cc1a36eea
dickm19 <maryclairedick@gmail.com>
2020-10-08 15:35:51 -0400

    Done

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,46 @@
 class DogsController < ApplicationController
+    before_action :find_dog, only: [:show, :edit, :update, :destory]
+
+    def index
+        @dogs = Dog.all
+    end
+
+    def show
+    end
+
+    def new
+        @dog = Dog.new
+    end
+
+    def create
+        @dog = Dog.create(dog_params)
+
+        redirect_to dog_path(@dog)
+    end
+
+    def edit
+
+    end
+   
+    def update
+        @dog.update(params.require(:dog).permit(:name, :breed, :age))
+
+        redirect_to dog_path(@dog)
+    end
+
+    def destory
+        @dog.destory
+        redirect_to dogs_path
+    end
+
+    
+    private
+
+    def dog_params
+        params.require(:dog).permit(:name, :breed, :age)
+    end
+
+    def find_dog
+        @dog = Dog.find(params[:id])
+    end
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,47 @@
 class EmployeesController < ApplicationController
+    before_action :find_employee, only: [:show, :edit, :update, :destory]
+
+    def index
+        @employees = Employee.all
+    end
+
+    def show
+    end
+
+    def edit
+
+    end
+
+    def new
+        @employee = Employee.new
+    end
+
+    def create
+        @employee = Employee.create(employee_params)
+
+        redirect_to employee_path(@employee)
+    end
+
+
+   
+    def update
+        @employee.update(params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id))
+        redirect_to employee_path(@employee)
+    end
+
+    def destory
+        @employee.destroy
+        redirect_to employees_path
+    end
+
+    
+    private
+
+    def employee_params
+        params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id)
+    end
+
+    def find_employee
+        @employee = Employee.find(params[:id])
+    end
 end
